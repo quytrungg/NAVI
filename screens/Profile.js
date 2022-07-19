b4203ef82cb72172218852f18e8b098f53bcca79
Quốc Anh <20127001@student.hcmus.edu.vn>
2022-07-19 23:30:01 +0700

    Profile Screen

---
+++ screens/Profile.js
@@ -0,0 +1,5 @@
+import React, {useState} from "react";
+import { SafeAreaView, View, ScrollView, Text, Image, FlatList, TouchableOpacity, Alert } from "react-native"
+import { TextInput } from "react-native-gesture-handler";
+import { COLORS, SIZES, FONTS, icons, images } from "../constants"
+
