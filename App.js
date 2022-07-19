38ded91baf8a8119098057532fed3a56b6c64740
yannickdell <81062333+yannickdell@users.noreply.github.com>
2022-07-19 23:27:16 +0700

    Tam's firebase config added

--- App.js
+++ App.js
@@ -24,23 +24,6 @@ const firebaseConfig = {
     appId: "1:1057415065385:web:5a839943956446ce51d138",
     measurementId: "G-9N3VJ0NH3Q"
 };
-//Tam's firebase
-// apiKey: "AIzaSyC3XhF93xG5JnJSzRbfl__WOnJyIK5HyCo",
-// authDomain: "fir-test-c9f0a.firebaseapp.com",
-// projectId: "fir-test-c9f0a",
-// storageBucket: "fir-test-c9f0a.appspot.com",
-// messagingSenderId: "1057415065385",
-// appId: "1:1057415065385:web:5a839943956446ce51d138",
-// measurementId: "G-9N3VJ0NH3Q"
-
-//Cien's firebase
-// apiKey: "AIzaSyD-U8hYj3kzXCYECo_5OWvtij5qB7unRD0",
-// authDomain: "instaclone-75dc2.firebaseapp.com",
-// projectId: "instaclone-75dc2",
-// storageBucket: "instaclone-75dc2.appspot.com",
-// messagingSenderId: "886924551037",
-// appId: "1:886924551037:web:f66ece62d64657abed2214",
-// measurementId: "G-SRJBL28KF6",
 
 if (firebase.apps.length === 0) {
   firebase.initializeApp(firebaseConfig);
@@ -62,7 +45,6 @@ const App = () => {
         "Roboto-Bold" : require('./assets/fonts/Roboto-Bold.ttf'),
         "Roboto-Regular" : require('./assets/fonts/Roboto-Regular.ttf'),
     })
-    
     if(!loaded){
     return null;
     }
@@ -83,8 +65,6 @@ const App = () => {
             </Stack.Navigator>
         </NavigationContainer>
     )
-    
 }
 
 export default App;
- 
