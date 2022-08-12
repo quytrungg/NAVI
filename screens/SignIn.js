ba62a2c12b309288ce5d13c64612473ccc02ebe6
HuynhNguyen0811 <huynhnguyen08112002@gmail.com>
2022-08-12 17:52:27 +0700

    Fix tab space #4

--- screens/SignIn.js
+++ screens/SignIn.js
@@ -1,6 +1,17 @@
-import React, {useState} from "react";
-import {View, Text, TouchableOpacity, Image, TextInput, KeyboardAvoidingView, Platform, Alert, StatusBar, ScrollView} from "react-native";
-import { LinearGradient } from 'expo-linear-gradient';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  TextInput,
+  KeyboardAvoidingView,
+  Platform,
+  Alert,
+  StatusBar,
+  ScrollView,
+} from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 import { COLORS, SIZES, FONTS, icons, images } from "../constants";
 
 import firebase from "firebase/compat/app";
@@ -9,98 +20,141 @@ import "firebase/compat/firestore";
 
 const SignIn = ({ navigation }) => {
   const [showPassword, setShowPassword] = useState(false);
-  const [phoneNumber, setPhoneNumber] = useState("")
-  const [password, setPassword] = useState("")
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [password, setPassword] = useState("");
 
-  function renderHeader(){
-    return(
-      <TouchableOpacity style={{flexDirection: 'row', 
-                                alignItems: "center", 
-                                marginTop: SIZES.padding * 6,
-                                paddingHorizontal: SIZES.padding * 2}}>
-        <Text style={{marginLeft: SIZES.padding,
-                      color: COLORS.blueback, 
-                      ...FONTS.h4 }}></Text>
-      </TouchableOpacity>
-    )
-  }
-
-  function renderLogo(){
-    return(
-      <View style={{marginTop: SIZES.padding * 5, 
-                    height: 90, 
-                    alignItems: 'center', 
-                    justifyContent: 'center'}}>
-        <Image  source={images.navilogo} 
-                resizeMode="contain" 
-                style={{width: "60%"}}/>
-      </View>
-    )
-  }
-
-  function renderForm(){
+  function renderHeader() {
     return (
-      <View style={{marginTop: SIZES.padding * 7, 
-                    marginHorizontal: SIZES.padding * 2}}>
-        <View style={{position: 'absolute', width: '100%', height:'100%', opacity: 0.5, borderRadius: 20}}></View>
-        <View style={{marginTop: SIZES.padding, marginHorizontal: SIZES.padding}}>
-          <Text style={{ color: COLORS.black, ...FONTS.body3 }}>Phone Number</Text>
-          <View style={{ flexDirection: 'row' }}>
-            <TextInput  style={{flex: 1,
-                                marginVertical: SIZES.padding,
-                                borderBottomColor: COLORS.black,
-                                borderBottomWidth: 1,
-                                height: 40,
-                                color: COLORS.black,
-                                ...FONTS.body3}}
-                        keyboardType="number-pad"
-                        placeholder="Enter Phone Number"
-                        placeholderTextColor={COLORS.gray}
-                        maxLength={11}
-                        selectionColor={COLORS.black}
-                        onChangeText={(phoneNumber) => setPhoneNumber(phoneNumber)}/>
+      <TouchableOpacity
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          marginTop: SIZES.padding * 6,
+          paddingHorizontal: SIZES.padding * 2,
+        }}
+      >
+        <Text
+          style={{
+            marginLeft: SIZES.padding,
+            color: COLORS.blueback,
+            ...FONTS.h4,
+          }}
+        ></Text>
+      </TouchableOpacity>
+    );
+  }
+
+  function renderLogo() {
+    return (
+      <View
+        style={{
+          marginTop: SIZES.padding * 5,
+          height: 90,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Image
+          source={images.navilogo}
+          resizeMode="contain"
+          style={{ width: "60%" }}
+        />
+      </View>
+    );
+  }
+
+  function renderForm() {
+    return (
+      <View
+        style={{
+          marginTop: SIZES.padding * 7,
+          marginHorizontal: SIZES.padding * 2,
+        }}
+      >
+        <View
+          style={{
+            position: "absolute",
+            width: "100%",
+            height: "100%",
+            opacity: 0.5,
+            borderRadius: 20,
+          }}
+        ></View>
+        <View
+          style={{ marginTop: SIZES.padding, marginHorizontal: SIZES.padding }}
+        >
+          <Text style={{ color: COLORS.black, ...FONTS.body3 }}>
+            Phone Number
+          </Text>
+          <View style={{ flexDirection: "row" }}>
+            <TextInput
+              style={{
+                flex: 1,
+                marginVertical: SIZES.padding,
+                borderBottomColor: COLORS.black,
+                borderBottomWidth: 1,
+                height: 40,
+                color: COLORS.black,
+                ...FONTS.body3,
+              }}
+              keyboardType="number-pad"
+              placeholder="Enter Phone Number"
+              placeholderTextColor={COLORS.gray}
+              maxLength={11}
+              selectionColor={COLORS.black}
+              onChangeText={(phoneNumber) => setPhoneNumber(phoneNumber)}
+            />
           </View>
         </View>
-        <View style={{marginTop: SIZES.padding * 2, marginHorizontal: SIZES.padding}}>
+        <View
+          style={{
+            marginTop: SIZES.padding * 2,
+            marginHorizontal: SIZES.padding,
+          }}
+        >
           <Text style={{ color: COLORS.black, ...FONTS.body3 }}>Password</Text>
-          <TextInput  style={{marginVertical: SIZES.padding,
-                              borderBottomColor: COLORS.black,
-                              borderBottomWidth: 1,
-                              height: 40,
-                              color: COLORS.black,
-                              ...FONTS.body3}}
-                      placeholder="Enter Password"
-                      placeholderTextColor={COLORS.gray}
-                      selectionColor={COLORS.black}
-                      secureTextEntry={!showPassword}
-                      onChangeText={(password) => setPassword(password)}/>
-          <TouchableOpacity style={{position: 'absolute',
-                                    right: 0,
-                                    bottom: 10,
-                                    height: 30,
-                                    width: 30}}
-                            onPress={() => setShowPassword(!showPassword)}>
-            <Image  source={showPassword ? icons.disable_eye : icons.eye}
-                    style={{height: 22,
-                            width: 22,
-                            tintColor: COLORS.black}}/>
+          <TextInput
+            style={{
+              marginVertical: SIZES.padding,
+              borderBottomColor: COLORS.black,
+              borderBottomWidth: 1,
+              height: 40,
+              color: COLORS.black,
+              ...FONTS.body3,
+            }}
+            placeholder="Enter Password"
+            placeholderTextColor={COLORS.gray}
+            selectionColor={COLORS.black}
+            secureTextEntry={!showPassword}
+            onChangeText={(password) => setPassword(password)}
+          />
+          <TouchableOpacity
+            style={{
+              position: "absolute",
+              right: 0,
+              bottom: 10,
+              height: 30,
+              width: 30,
+            }}
+            onPress={() => setShowPassword(!showPassword)}
+          >
+            <Image
+              source={showPassword ? icons.disable_eye : icons.eye}
+              style={{ height: 22, width: 22, tintColor: COLORS.black }}
+            />
           </TouchableOpacity>
         </View>
       </View>
-    )
+    );
   }
 
-  function handleForgetPassword(){
+  function handleForgetPassword() {
     if (phoneNumber == "") {
-      Alert.alert(
-        "Error",
-        "Need phone number to reset password.",
-        [
-          {
-            text: "Retry",
-          },
-        ]
-      );
+      Alert.alert("Error", "Need phone number to reset password.", [
+        {
+          text: "Retry",
+        },
+      ]);
     } else {
       firebase
         .firestore()
@@ -113,21 +167,16 @@ const SignIn = ({ navigation }) => {
               .auth()
               .sendPasswordResetEmail(snapshot.data().email)
               .then(() => {
-                Alert.alert(
-                  "",
-                  "Password reset email sent!",
-                  [
-                    {
-                      text: "OK",
-                      onPress: () => {
-                        navigation.navigate("SignIn")
-                      }
-                    }
-                  ]
-                )
-              })
-          }
-          else {
+                Alert.alert("", "Password reset email sent!", [
+                  {
+                    text: "OK",
+                    onPress: () => {
+                      navigation.navigate("SignIn");
+                    },
+                  },
+                ]);
+              });
+          } else {
             Alert.alert(
               "Error",
               "Account does not exist. Do you wish to create a new account?",
@@ -135,7 +184,7 @@ const SignIn = ({ navigation }) => {
                 {
                   text: "Yes",
                   onPress: () => {
-                      navigation.navigate("SignUp");
+                    navigation.navigate("SignUp");
                   },
                 },
                 {
@@ -144,7 +193,7 @@ const SignIn = ({ navigation }) => {
               ]
             );
           }
-        })
+        });
     }
   }
 
@@ -157,7 +206,7 @@ const SignIn = ({ navigation }) => {
           {
             text: "Retry",
             onPress: () => {
-                navigation.navigate("SignIn");
+              navigation.navigate("SignIn");
             },
           },
         ]
@@ -170,108 +219,140 @@ const SignIn = ({ navigation }) => {
           navigation.navigate("HomeAdmin", {
             username: "admin",
           });
-        })
+        });
     } else {
       firebase
-      .firestore()
-      .collection("user")
-      .doc(phoneNumber)
-      .get()
-      .then((snapshot) => {
-        if (snapshot != undefined) {
-          firebase
-          .auth()
-          .signInWithEmailAndPassword(snapshot.data().email, password)
-          .then(() => {
-            setPhoneNumber("")
-            setPassword("")
-            navigation.navigate("Loading", {
-              username: snapshot.data().name,
-              phoneNumber: snapshot.data().phoneNumber,
-            });
-          })
-          .catch(() => {
-            Alert.alert(
-              "Error",
-              "Wrong phone number/password. Please try again",
-              [
-                {
-                  text: "Retry",
-                  onPress: () => {
-                    handleSignIn();
-                  },
+        .firestore()
+        .collection("user")
+        .doc(phoneNumber)
+        .get()
+        .then((snapshot) => {
+          if (snapshot != undefined) {
+            firebase
+              .auth()
+              .signInWithEmailAndPassword(snapshot.data().email, password)
+              .then(() => {
+                setPhoneNumber("");
+                setPassword("");
+                navigation.navigate("Loading", {
+                  username: snapshot.data().name,
+                  phoneNumber: snapshot.data().phoneNumber,
+                });
+              })
+              .catch(() => {
+                Alert.alert(
+                  "Error",
+                  "Wrong phone number/password. Please try again",
+                  [
+                    {
+                      text: "Retry",
+                      onPress: () => {
+                        handleSignIn();
+                      },
+                    },
+                    {
+                      text: "OK",
+                    },
+                  ]
+                );
+              });
+          }
+        })
+        .catch(() => {
+          Alert.alert(
+            "Error",
+            "Wrong phone number/password. Please try again",
+            [
+              {
+                text: "Retry",
+                onPress: () => {
+                  handleSignIn();
                 },
-                {
-                  text: "OK",
-                },
-              ]
-            );
-          }) 
-        }
-      })
-      .catch(() => {
-        Alert.alert(
-          "Error",
-          "Wrong phone number/password. Please try again",
-          [
-            {
-              text: "Retry",
-              onPress: () => {
-                handleSignIn();
               },
-            },
-            {
-              text: "OK",
-            },
-          ]
-        );
-      }) 
+              {
+                text: "OK",
+              },
+            ]
+          );
+        });
     }
-    
   }
-  
-  function renderButton(){
-    return(
+
+  function renderButton() {
+    return (
       <View style={{ margin: SIZES.padding * 5 }}>
-        <TouchableOpacity style={{height: 60,
-                                  width: 180,
-                                  alignSelf: "center",
-                                  backgroundColor: COLORS.bluesec,
-                                  borderRadius: SIZES.radius / 1.5,
-                                  alignItems: 'center',
-                                  justifyContent: 'center',
-                                  borderColor: COLORS.blueprim}}
-                          onPress={() => handleSignIn()}>
-          <Text style={{color: COLORS.white, ...FONTS.h3}}>Sign In</Text>
+        <TouchableOpacity
+          style={{
+            height: 60,
+            width: 180,
+            alignSelf: "center",
+            backgroundColor: COLORS.bluesec,
+            borderRadius: SIZES.radius / 1.5,
+            alignItems: "center",
+            justifyContent: "center",
+            borderColor: COLORS.blueprim,
+          }}
+          onPress={() => handleSignIn()}
+        >
+          <Text style={{ color: COLORS.white, ...FONTS.h3 }}>Sign In</Text>
         </TouchableOpacity>
-        <View style={{marginTop: SIZES.padding / 1.5,
-                      marginBottom: SIZES.padding * 5, 
-                      alignSelf: "center"}}>
-          <TouchableOpacity onPress = {() => handleForgetPassword()}>
-            <Text style={{color: COLORS.bluetext, 
-                          ...FONTS.h4body, 
-                          textDecorationLine: 'underline'}}>Forget password?</Text>
+        <View
+          style={{
+            marginTop: SIZES.padding / 1.5,
+            marginBottom: SIZES.padding * 5,
+            alignSelf: "center",
+          }}
+        >
+          <TouchableOpacity onPress={() => handleForgetPassword()}>
+            <Text
+              style={{
+                color: COLORS.bluetext,
+                ...FONTS.h4body,
+                textDecorationLine: "underline",
+              }}
+            >
+              Forget password?
+            </Text>
           </TouchableOpacity>
         </View>
-        <View style={{margin: SIZES.padding / 2, flexDirection: 'row', alignSelf: "center"}}>
-          <View style={{alignSelf: "center"}}>
-            <Text style={{color: COLORS.bluetext, ...FONTS.h4body}}>Don't have an account? </Text>
+        <View
+          style={{
+            margin: SIZES.padding / 2,
+            flexDirection: "row",
+            alignSelf: "center",
+          }}
+        >
+          <View style={{ alignSelf: "center" }}>
+            <Text style={{ color: COLORS.bluetext, ...FONTS.h4body }}>
+              Don't have an account?{" "}
+            </Text>
           </View>
-          <TouchableOpacity onPress = {() => navigation.navigate("SignUp")}>
-            <Text style={{color: COLORS.bluesec, 
-                          ...FONTS.h4, 
-                          textDecorationLine: 'underline'}}>Sign Up</Text>
+          <TouchableOpacity onPress={() => navigation.navigate("SignUp")}>
+            <Text
+              style={{
+                color: COLORS.bluesec,
+                ...FONTS.h4,
+                textDecorationLine: "underline",
+              }}
+            >
+              Sign Up
+            </Text>
           </TouchableOpacity>
         </View>
       </View>
-    )
+    );
   }
 
-  return(
-    <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : null}
-                          style={{flex: 1}}>
-      <LinearGradient colors={[COLORS.blueback, COLORS.blueback]} style={{flex:1}}>
-        <StatusBar barStyle = "dark-content" hidden = {false} translucent = {true}/>
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : null}
+      style={{ flex: 1 }}
+    >
+      <LinearGradient
+        colors={[COLORS.blueback, COLORS.blueback]}
+        style={{ flex: 1 }}
+      >
+        <StatusBar barStyle="dark-content" hidden={false} translucent={true} />
         <ScrollView>
           {renderHeader()}
           {renderLogo()}
@@ -280,7 +361,7 @@ const SignIn = ({ navigation }) => {
         </ScrollView>
       </LinearGradient>
     </KeyboardAvoidingView>
-  )
-}
+  );
+};
 
 export default SignIn;
